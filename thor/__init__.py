a4b521a5346e8684f3bb0069b6f9a5a7132ba232
Eoghan <eoghan@gaeilgemail.com>
2024-10-03 20:47:58 +0100

    Add basic files

--- thor/__init__.py
+++ thor/__init__.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,
+                static_url_path = '')       
 
     @app.route('/')
     def index():
-        return 'Hello, World!'
+        return render_template('base.html')
 
     return app
 
+
+if __name__ == '__main__':
+    debug_app = create_app()
+    debug_app.run()
