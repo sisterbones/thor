d37842ebdef824673db3d0cc66e196f24a95aed3
Eoghan <eoghan@gaeilgemail.com>
2024-10-04 16:08:03 +0100

    fix install script (actually have up to date dependencies)

--- thor/__init__.py
+++ thor/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__,
-                static_url_path = '')       
+                static_url_path = '')
 
     @app.route('/')
     def index():
